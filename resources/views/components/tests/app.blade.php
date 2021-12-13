c221d102b141ca9c9cdf70c7e8f124b2d47b920b
hongo <zhetaibenxiang@gmail.com>
2021-12-13 11:21:24 +0900

    名前付きスロット

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -17,6 +17,8 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
+        <header>
+            {{ $header }}
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,5 +1,6 @@
 <x-tests.app>
 
+<x-slot name="header">header2</x-slot>
 コンポーネントテスト2
 
 </x-tests.app>
