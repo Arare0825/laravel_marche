c08ba3d7ebcab860ff104d173875da93d77c3bab
hongo <zhetaibenxiang@gmail.com>
2022-01-24 15:44:21 +0900

    '画像リサイズ設定追加'

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
+use Illuminate\Support\Facades\Storage;
+use InterventionImage;
 
 class ShopController extends Controller
 {
@@ -37,7 +39,7 @@ class ShopController extends Controller
     {
     //    $ownerId = Auth::id();
        $shops = Shop::where('owner_id', Auth::id())->get();
-
+// phpinfo();
        return view('owner.shops.index',
                     compact('shops'));
 
@@ -45,11 +47,25 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.shops.edit',compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
+     $imageFile = $request->image;
+     if(!is_null($imageFile) && $imageFile->isValid() ){
+        //  Storage::putFile('public/shops', $imageFile); //リサイズなしの場合
+        $fileName = uniqid(rand().'_');
+        $extension = $imageFile->extension();
+        $fileNameToStore = $fileName. '.' . $extension;
+        $resizedImage = InterventionImage::make($imageFile)->resize(1920,1080)->encode();
+// dd($imageFile,$resizedImage);
+
+        Storage::put('public/shops/' . $fileNameToStore,$resizedImage);
+     }
+     return redirect()->route('owner.shops.index');
     }
 
 }
