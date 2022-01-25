6e8c7c58f3b0e7ec9588741bd062452ccf06ff1b
hongo <zhetaibenxiang@gmail.com>
2022-01-25 21:15:37 +0900

    '諸々修正'

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -67,9 +67,9 @@ class OwnersController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:owners',
+            'password' => 'required|string|confirmed|Rules\Password::defaults()',
         ]);
 
         try{
