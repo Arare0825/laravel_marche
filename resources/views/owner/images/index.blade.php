d1d5c5e4837909c1e7c19ce33e29aa36ab870456
hongo <zhetaibenxiang@gmail.com>
2022-01-27 21:20:12 +0900

    'imagetitleの作成'

--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -15,11 +15,12 @@
                     </div>
                     <div class="flex flex-wrap">
                     @foreach($images as $image)
-                    <div class="w-1/4 p-4">
+                    <div class="w-1/4 p-2 md:p-4">
                     <a href="{{ route('owner.images.edit',['image'=>$image->id ])}}">
-                        <div class="border rounded-md p-4">
+                        <div class="border rounded-md p-2 md:p-4">
                         <x-thumbnail :filename="$image->filename" type="products" />
-                         {{ $image->title }}
+                        <div class="text-gray-700"> {{ $image->title }}
+                        </div>
                         </div>
                    </a>
                    </div>
@@ -31,3 +32,4 @@
             </div>
         </div>
 </x-app-layout>
+
