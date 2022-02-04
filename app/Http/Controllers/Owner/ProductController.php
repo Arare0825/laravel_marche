be7809a4b14c0ebd64c35e6c028a6b8be62e4557
hongo <zhetaibenxiang@gmail.com>
2022-02-04 18:10:35 +0900

    'EditUpdateおわり

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -162,7 +162,7 @@ class ProductController extends Controller
 
         if($request->current_quantity !== $quantity){
             $id = $request->route()->parameter('product');
-            return redirect()->route('owner.product.edit',['product'=>$id])
+            return redirect()->route('owner.products.edit',['product'=>$id])
             ->with(['message','在庫数が変更されました','status'=>'alert']);
         }else{
             try{
@@ -180,10 +180,10 @@ class ProductController extends Controller
                         $product->is_selling = $request->is_selling;
                         $product->save();
 
-                        if($request->type === '1'){
+                        if($request->type === \Constant::PRODUCT_LIST['add']){
                             $newQuantity = $request->quantity;
                         }
-                        if($request->type === '2'){
+                        if($request->type === \Constant::PRODUCT_LIST['reduce']){
                             $newQuantity = $request->quantity * -1;
                         }
                     Stock::create([
