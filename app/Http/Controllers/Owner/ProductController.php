62b8a3aa3fb08eabb533122c3ace9bf1712d4889
hongo <zhetaibenxiang@gmail.com>
2022-02-04 18:19:13 +0900

    'destroy'

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -211,14 +211,15 @@ class ProductController extends Controller
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        Product::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('owner.products.index')
+        ->with(['message'=>'商品情報を削除しました',
+                 'status'=>'alert']);
+ 
+
     }
 }
